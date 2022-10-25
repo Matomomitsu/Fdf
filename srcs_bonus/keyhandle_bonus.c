639aef4ccf9708fb6d4fc58ea92c508bbceff25c
Mateus Tomomitsu <matomomitsu@gmail.com>
2022-10-25 02:28:19 -0300

    fix Key_left

--- srcs_bonus/keyhandle_bonus.c
+++ srcs_bonus/keyhandle_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mtomomit <mtomomit@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/25 06:50:57 by mtomomit          #+#    #+#             */
-/*   Updated: 2022/09/02 16:40:44 by mtomomit         ###   ########.fr       */
+/*   Updated: 2022/10/25 02:27:54 by mtomomit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ int	key_handle(int keycode, t_fdf *fdf)
 		reset(fdf->i_point, fdf);
 	if (keycode == KEY_UP || keycode == KEY_DOWN)
 		rotation_x(fdf->i_point, fdf, keycode);
-	if (keycode == KEY_RIGHT || keycode == KEY_RIGHT)
+	if (keycode == KEY_RIGHT || keycode == KEY_LEFT)
 		rotation_y(fdf->i_point, fdf, keycode);
 	if (keycode == KEY_M || keycode == KEY_N)
 		rotation_z(fdf->i_point, fdf, keycode);
